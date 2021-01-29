8f7f6234a482891d1e7d0bedde527fea00ae357f
Sarah Mucho <64818980+SJMucho@users.noreply.github.com>
2021-01-28 22:03:14 -0500

    form is started

--- how-to/src/Components/Register.js
+++ how-to/src/Components/Register.js
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from "react";
+import * as yup from "yup";
 
 const Register = () => {
   const [formData, setFormData] = useState({
-    name: "",
+    fname: "",
+    lname: "",
     email: "",
     username: "",
     password: "",
@@ -10,7 +12,54 @@ const Register = () => {
     userRole: "",
   });
 
-  return <div>Register</div>;
+  const [errors, setErrors] = useState({
+    fname: "",
+    lname: "",
+    email: "",
+    username: "",
+    password: "",
+    confirmPassword: "",
+    userRole: "",
+  });
+
+  const validateChange = (e) => {
+    e.persist();
+    yup
+      .reach(formSchema, e.target.name)
+      .validate(e.target.value)
+      .then((valid) => {
+        setErrors({ ...errors, [e.target.name]: "" });
+      })
+      .catch((err) => {
+        setErrors({ ...errors, [e.target.name]: err.errors[0] });
+      });
+  };
+
+  const onInputChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+    validateChange(e);
+  };
+
+  const formSchema = yup.object().shape({
+    fname: yup.string().required("Frist name required"),
+  });
+
+  return (
+    <form>
+      <div>Register here</div>
+      <input
+        onChange={onInputChange}
+        name="fname"
+        id="fname"
+        type="text"
+        value={formData.fname}
+        placeholder="First name"
+      />
+    </form>
+  );
 };
 
 export default Register;
