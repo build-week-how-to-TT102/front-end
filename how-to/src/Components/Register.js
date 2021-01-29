bd5337f359ff6c963617517450133ff3b3d0c15c
Sarah Mucho <64818980+SJMucho@users.noreply.github.com>
2021-01-28 20:58:28 -0500

    register component created

---
+++ how-to/src/Components/Register.js
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from "react";
+
+const Register = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    username: "",
+    password: "",
+    confirmPassword: "",
+    userRole: "",
+  });
+
+  return <div>Register</div>;
+};
+
+export default Register;
