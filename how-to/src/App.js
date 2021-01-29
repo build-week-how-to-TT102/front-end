632679dabeff3df0025a5c8abfc039cda0a8ea16
Molly Brown <mollybrown0516@gmail.com>
2021-01-28 20:12:17 -0500

    Completed App.js routing to Main(Homepage), Login, and SubmitPost components.

--- how-to/src/App.js
+++ how-to/src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch };
+//importing components
+import Main from './Components/Main';
+import Login from './Components/Login';
+import SubmitPost from './Components/SubmitPost';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>How-To</h1>
+        <h2>Life Hacks</h2>
+          <div className="App">
+            <Router>
+              <Switch>
+
+                <Route exact path={'/'}>
+                  <Main/> {/*Only routing, no props*/}
+                </Route>
+
+                <Route path={'/Login'}>
+                  <Login/>
+                </Route>
+
+                <Route path={'/SubmitPost'}>
+                  <SubmitPost/>
+                </Route>
+
+              </Switch>
+            </Router>
+          
+          </div>
+    </>
   );
 }
 
